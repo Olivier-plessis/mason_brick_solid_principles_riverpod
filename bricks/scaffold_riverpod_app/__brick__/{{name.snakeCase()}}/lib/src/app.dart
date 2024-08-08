81e724c650c9c7f6c7f81c71f2bbd45ce8bc23da
olivier_plessis <hello@olivierplessis.fr>
2024-08-08 16:55:36 +0200

    add route auto

--- bricks/scaffold_riverpod_app/__brick__/{{name.snakeCase()}}/lib/src/app.dart
+++ bricks/scaffold_riverpod_app/__brick__/{{name.snakeCase()}}/lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:{{name.snakeCase()}}/src/core/theme/dark_theme.dart';
 import 'package:{{name.snakeCase()}}/src/core/theme/light_theme.dart';
 import 'package:{{name.snakeCase()}}/src/core/theme/theme_mode_controller.dart';
