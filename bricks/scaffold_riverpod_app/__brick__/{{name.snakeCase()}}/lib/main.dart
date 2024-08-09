e044a105eaca0d33d9392c1e6de111cf37db29c6
olivier_plessis <hello@olivierplessis.fr>
2024-08-09 09:53:05 +0200

    refactor

--- bricks/scaffold_riverpod_app/__brick__/{{name.snakeCase()}}/lib/main.dart
+++ bricks/scaffold_riverpod_app/__brick__/{{name.snakeCase()}}/lib/main.dart
@@ -10,23 +10,6 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 {{/hooks}}
 import 'package:{{name.snakeCase()}}/src/core/utils/observers.dart';
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // turn off the # in the URLs on the web
-  usePathUrlStrategy();
-  // ensure URL changes in the address bar when using push / pushNamed
-  // more info here: https://docs.google.com/document/d/1VCuB85D5kYxPR3qYOjVmw8boAGKb7k62heFyfFHTOvw/edit
-  GoRouter.optionURLReflectsImperativeAPIs = true;
-
-  runApp(
-    ProviderScope(
-      observers: <ProviderObserver>[
-        Observers(),
-      ],
-      child: const MainApp(),
-    ),
-  );
-}
 
 Future<void> main() async {
   // turn off the # in the URLs on the web and remove if only mobil app development
